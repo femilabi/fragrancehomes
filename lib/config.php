c158e8c9b1d06538f100aaa2f9099003e0a19703
femilabi <35839581+femilabi@users.noreply.github.com>
2021-05-17 17:16:40 +0100

    Add files via upload
    new config

--- lib/config.php
+++ lib/config.php
@@ -1,6 +1,6 @@
 <?php
 //Paths
-define("APP_PROTOCOL", "http://");
+define("APP_PROTOCOL", "https://");
 define("HOST", $_SERVER["HTTP_HOST"]);
 define("HOME_DIR", APP_PROTOCOL . HOST . "/"); //App home page URL
 define("BASE_DIR", HOME_DIR); //Parent App URL if any
@@ -9,7 +9,7 @@ define("CDN_DIR", BASE_DIR . "cdn/"); //HTML Base be provided in base tag
 
 define("SELF_PATH", dirname(__DIR__) . "/"); // Path to App roots dir (same as SYS_PATH if app not a child
 define("SYS_PATH", SELF_PATH); //Path to Parent App Dir if any (if no parent, App's root dir
-define("GLOBAL_PATH", dirname(SYS_PATH) . "/global/"); //Path to Network global Dir
+define("GLOBAL_PATH", SELF_PATH . "global/"); //Path to Network global Dir
 
 define("INCLUDE_PATH", SELF_PATH . "includes/");
 define("CLASS_PATH", GLOBAL_PATH . "classes/");
@@ -19,10 +19,10 @@ define("WEBAPP_CONFIG", INCLUDE_PATH . "app/app_config.php");
 
 //Database
 define("DB_PREFIX", "fh_");
-define("DB_NAME", "fraghomes");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_HOST", "localhost");
+define("DB_NAME", "gethwjpj_server");
+define("DB_USER", "gethwjpj_server");
+define("DB_PASS", "^r(L89r@Rmsb");
+define("DB_HOST", "gethsempray.org");
 
 //Session
 define("USER_SESSION_HOLDER", "app_user");
@@ -34,13 +34,13 @@ define("TITLE_SUFFIX", " | Fragrance Homes & Realtors");
 //Email constants
 define("ALLOW_EMAIL", 0);
 define("ADMIN_NAME", "Fragrance Homes & Realtors");
-define("ADMIN_EMAIL", "no_reply@ztpcloud.com");
-define("ADMIN_DOMAIN", "example.com");
-define("SMTP_HOST", "mail.example.com");
-define("SMTP_PORT", 26);
-define("SMTP_ENCRYPT", ""); //"ssl", "tls"
-define("SMTP_USERNAME", "no_reply@ztpcloud.com");
-define("SMTP_PASSWORD", "...");
+define("ADMIN_EMAIL", "server@c4cgrill.com");
+define("ADMIN_DOMAIN", "c4cgrill.com");
+define("SMTP_HOST", "gethsempray.org");
+define("SMTP_PORT", 465);
+define("SMTP_ENCRYPT", "ssl"); //"ssl", "tls"
+define("SMTP_USERNAME", "server@c4cgrill.com");
+define("SMTP_PASSWORD", "A\$smYVU=WvH*");
 
 //reCaptcha Keys
 define("RECAPTCHA_PUB_KEY", "...");
