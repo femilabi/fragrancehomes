acb4d1224d8b9b32477649079444772dfcbd8a38
Oderinwale Oluwafemi Peter <oderinwalefemi150@gmail.com>
2021-05-18 00:58:36 +0100

    Pushing correct dev

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if (isset($_SESSION[USER_SESSION_HOLDER]) && is_array($_SESSION[USER_SESSION_HOL
 	$USER = new User($_SESSION[USER_SESSION_HOLDER]["id"]);
 }
 
-var_dump($DB->get_query_set("SHOW TABLES"));
+echo json_encode($DB->get_query_set("SHOW TABLES"));
 exit;
 
 //Get content type header for output
